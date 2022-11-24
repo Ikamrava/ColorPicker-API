1123801d9fa8a3557a35194c33eb34d957f851d5
ikamrava <ikamrava>
2022-11-24 23:30:50 +0000

    final

--- index.js
+++ index.js
@@ -1,4 +1,65 @@
 
-fetch("https://www.thecolorapi.com/scheme?hex=0047AB&rgb=0,71,171&hsl=215,100%,34%&cmyk=100,58,0,33&format=html&mode=analogic&count=6")
-.then((response) => response.json())
-.then((data) => console.log(data));
+
+function getColors(){
+    const mode = document.getElementById("mode")
+    const sampleColor = document.getElementById("colorsample")
+    const color = sampleColor.value.slice(1)
+    const url = `https://www.thecolorapi.com/scheme?hex=${color}&mode=${mode.value}&count=10`
+    const colorArray = []
+
+    fetch(url)
+    .then(res => res.json())
+    .then(function(data){
+        let fullData = data.colors
+        for (let i=0;i<fullData.length;i++){
+            colorArray.push(fullData[i].hex.value)
+        }
+        document.getElementById("palletContainer").innerHTML = render(colorArray)
+    })
+}
+    
+
+    document.getElementById("getcolor").addEventListener("click",function(){
+        getColors()
+    
+    })
+
+    function render(list){
+      let htmlTag = ""
+      for (let i = 0;i<list.length;i++){
+        htmlTag += `
+        <div id="${list[i]}" class="palletdiv" style="background-color:${list[i]}" >
+        <span id="${list[i]}">${list[i]}</span>
+      </div>
+      ` 
+      }
+       
+      
+        return htmlTag
+
+    }
+
+    document.getElementById("palletContainer").addEventListener("click", function(e){
+      const snakBar = document.getElementById("snackbar");
+      navigator.clipboard.writeText(e.target.id);
+      snakBar.textContent = `${e.target.id} Copied`
+      snakBar.className = "show";
+      setTimeout(function () {
+        snakBar.className = snakBar.className.replace("show", "");
+      }, 3000);
+     
+    })
+
+
+
+  
+
+
+
+    getColors()
+
+    
+    
+
+
+    
