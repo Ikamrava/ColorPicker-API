1b5503e1e607c06c43f7141a8021313bee7ab2b1
ikamrava <ikamrava>
2022-11-24 16:30:29 +0000

    test

---
+++ index.js
@@ -0,0 +1,4 @@
+
+fetch("https://www.thecolorapi.com/scheme?hex=0047AB&rgb=0,71,171&hsl=215,100%,34%&cmyk=100,58,0,33&format=html&mode=analogic&count=6")
+.then((response) => response.json())
+.then((data) => console.log(data));
